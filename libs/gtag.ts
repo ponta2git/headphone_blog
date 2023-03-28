b2bfc29442bc1fe09c0f121643f8564b38a2fc1f
ponta <coshun@gmail.com>
2023-03-28 22:25:11 +0900

    fix tracking id prefix.

--- libs/gtag.ts
+++ libs/gtag.ts
@@ -2,7 +2,7 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || "";
 
 export const pageView = (path: string) => {
   if (typeof window !== undefined) {
-    window.gtag("config", GA_TRACKING_ID, {
+    window.gtag("config", `G-${GA_TRACKING_ID}`, {
       page_path: path,
     });
   }
