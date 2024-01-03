c66281f5186d2e3cad38385b1221a8d813fb095d
ponta <coshun@gmail.com>
2024-01-04 03:14:37 +0900

    revise container and footer.

--- src/components/PageElements/Container.tsx
+++ src/components/PageElements/Container.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react"
 
 export default function Container({ children }: { children?: ReactNode }) {
   return (
-    <main className="relative z-0 mb-24 w-screen bg-slate-50 px-8 pb-12 pt-10 leading-8 tracking-wide text-neutral-700 md:mx-auto md:w-3/4 md:rounded lg:w-3/5">
+    <main className="relative z-0 mb-24 w-screen bg-slate-50 px-8 pb-12 pt-10 leading-8 tracking-wide text-neutral-700 md:mx-auto md:w-3/4 md:rounded-xl lg:w-3/5">
       {children}
     </main>
   )
--- src/components/PageElements/Footer.tsx
+++ src/components/PageElements/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   const year = DateTime.now().year
 
   return (
-    <footer className="w-screen bg-slate-50 py-6">
+    <footer className="w-screen bg-slate-50 py-8">
       <div className="flex flex-col gap-y-1 text-center text-sm tracking-[0.4px] text-[#7b8ca2]">
         <div className="mb-2 flex flex-row justify-center gap-x-6">
           <a href="mailto:coshun@gmail.com" className="h-4 w-4">
