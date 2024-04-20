103bd69aa35724adfb488040f13b079a5cb69d8c
ponta <coshun@gmail.com>
2024-04-20 23:00:48 +0900

    delete "as const" in siteBasic

--- src/siteBasic.ts
+++ src/siteBasic.ts
@@ -1,5 +1,5 @@
-export const siteName = "pontaのヘッドホンブログ" as const
+export const siteName = "pontaのヘッドホンブログ"
 export const siteDescription =
-  "ヘッドホンオーディオを楽しんでいます。ヘッドホンや機材のインプレッションやヘッドホンオーディオの楽しみ方などについて気楽に書き連ねています。" as const
-export const siteUrl = "https://ponta-headphone.net/" as const
-export const postDir = "posts" as const
+  "ヘッドホンオーディオを楽しんでいます。ヘッドホンや機材のインプレッションやヘッドホンオーディオの楽しみ方などについて気楽に書き連ねています。"
+export const siteUrl = "https://ponta-headphone.net/"
+export const postDir = "posts"
