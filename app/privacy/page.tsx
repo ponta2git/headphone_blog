4c1621ff486ef784471933463f7d100e5794b41d
ponta <coshun@gmail.com>
2025-02-15 14:52:55 +0900

    remove content class.

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -27,15 +27,15 @@ export default function Page() {
       </div>
 
       <div className="mx-2 my-10 mt-6 flex flex-col gap-4 lg:px-2">
-        <p className="content">
+        <p>
           pontaのヘッドホンブログ（以下、当サイト）におけるプライバシーポリシーは次の通りです。
         </p>
         <h2 className="font-semibold">日本国の法令に基づく個人情報の保護</h2>
-        <p className="content">
+        <p>
           当サイトにおける個人情報の保護は、日本国の法令に基づいて行われることとします。
         </p>
         <h2 className="font-semibold">アクセス解析ツールの使用</h2>
-        <p className="content">
+        <p>
           当サイトでは、Googleによるアクセス解析ツール、Google
           Analyticsを使用しています。Google
           Analyticsは、アクセスデータの収集のためにCookieを使用しています。このデータは匿名で収集されており、個人を特定するものではありません。Cookieを無効にすることで収集を拒否できますので、お使いのブラウザの設定をご確認ください。詳細は、
@@ -49,15 +49,15 @@ export default function Page() {
         </p>
 
         <h2 className="font-semibold">免責事項</h2>
-        <p className="content">
+        <p>
           当サイトでは、当サイトの掲載内容によって生じた損害に対する一切の責任を負いません。また、可能な限り注意を払った情報提供を心がけておりますが、正確性や安全性を保証するものではありません。また、リンク先の他サイトで提供される情報・サービスにつきましても一切の責任を負いません。
         </p>
         <h2 className="font-semibold">プライバシーポリシーの改定</h2>
-        <p className="content">
+        <p>
           当サイトは、本プライバシーポリシーを予告なく改定する事があります。修正された最新のプライバシーポリシーは、常に本ページにて公開されます。
         </p>
-        <p className="content">以上</p>
-        <p className="content">
+        <p>以上</p>
+        <p>
           連絡先：ponta
           <address>
             <a
