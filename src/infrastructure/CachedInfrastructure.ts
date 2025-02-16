38cfe4366739aee054ca3eef5159184ebff53b5e
ponta <coshun@gmail.com>
2025-02-16 23:38:49 +0900

    remove unnecessary items and move some tsxs.

--- src/infrastructure/CachedInfrastructure.ts
+++
@@ -1,7 +0,0 @@
-import { cache } from "react";
-
-import { getAllPostDates } from "./PostDateRepository";
-import { findPostByDate } from "./PostRepository";
-
-export const getAllPostDatesWithCache = cache(getAllPostDates);
-export const findPostByDateWithCache = cache(findPostByDate);
