2fea9fa0a3de2b63e262ec823993ab981ba83d43
ponta <coshun@gmail.com>
2025-04-08 23:38:52 +0900

    remove unused directives.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 import typescript from "typescript-eslint";
 import { FlatCompat } from "@eslint/eslintrc";
 import prettierConfig from "eslint-config-prettier";
