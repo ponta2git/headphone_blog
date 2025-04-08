84d45acc39a31880114323d5c3c57fd10530090a
ponta <coshun@gmail.com>
2025-04-08 23:39:59 +0900

    chore: replace logo.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,19 +3,17 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
-      <div className="w-full">
-        <Link href="/">
-          <Image
-            src={"/images/headphone_blog_logo.png"}
-            width={230}
-            height={230}
-            alt="pontaのヘッドホンブログ"
-            priority
-            className="mx-auto block"
-          />
-        </Link>
-      </div>
+    <header className="w-full">
+      <Link href="/">
+        <Image
+          src={"/images/logo.webp"}
+          width={230}
+          height={230}
+          alt="pontaのヘッドホンブログ"
+          priority
+          className="mx-auto block"
+        />
+      </Link>
     </header>
   );
 }
