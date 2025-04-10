82222d7ed4bc63c5a5d5a4388f775c6922212e82
ponta <coshun@gmail.com>
2025-04-10 22:02:28 +0900

    chore: fix markup.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,16 +4,18 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="w-full">
-      <Link href="/">
-        <Image
-          src={"/images/logo.webp"}
-          width={230}
-          height={230}
-          alt="pontaのヘッドホンブログ"
-          priority
-          className="mx-auto block"
-        />
-      </Link>
+      <p className="mx-auto w-[230px]">
+        <Link href="/">
+          <Image
+            src={"/images/logo.webp"}
+            width={230}
+            height={230}
+            alt="pontaのヘッドホンブログ"
+            priority
+            className="duration-100 hover:opacity-80 hover:transition-opacity"
+          />
+        </Link>
+      </p>
     </header>
   );
 }
