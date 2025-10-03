0db31d27bca1a51a1a72fcba92283d48be086337
ponta <coshun@gmail.com>
2025-10-03 21:21:00 +0900

    enable typedEnv.

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,9 +4,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly ENABLE_GTM?: string;
-  }
-}
--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,9 @@ const nextConfig: NextConfig = {
   output: "export",
   typedRoutes: true,
   images: { unoptimized: true },
+  experimental: {
+    typedEnv: true,
+  },
 };
 
 export default process.env.ANALYZE
